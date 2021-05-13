014b5381e17dd2fd924f30cfd91d6d6f677a50ef
lixiaofeng <v-lixiaofeng@immotors.com>
2021-05-13 18:56:40 +0800

    init

---
+++ common/springsecurity/src/main/java/com/jimloop/security/security/UnauthEntryPoint.java
@@ -0,0 +1,18 @@
+package com.jimloop.security.security;
+
+import com.jimloop.utils.utils.R;
+import com.jimloop.utils.utils.ResponseUtil;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class UnauthEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+        ResponseUtil.out(httpServletResponse, R.error());
+    }
+}
